ede47bbe20765150f9b530b8873f4e788fb74e3c
tranthinh201 <console.namee@gmail.com>
2024-01-03 16:30:58 +0700

    feat: present payment stripe

--- src/screens/Booking/PaymentScreen/PaymentScreen.tsx
+++ src/screens/Booking/PaymentScreen/PaymentScreen.tsx
@@ -1,19 +1,27 @@
+import { createTransaction } from '@/libs/api/booking'
 import { getSeatMovie } from '@/libs/api/movie'
+import { createIntent } from '@/libs/api/payment'
 import { Header } from '@/libs/components'
 import { PaymentSkeleton } from '@/libs/components/Skeleton/Payment'
 import { RouteBookingStackType } from '@/libs/route'
-import { useRoute } from '@react-navigation/native'
-import { useQuery } from '@tanstack/react-query'
+import { NavigationProp } from '@/navigation'
+import { RootStore } from '@/store'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import { useStripe } from '@stripe/stripe-react-native'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { isEqual } from 'lodash'
 import { useState } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { Alert, StyleSheet, View } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
 import { Button, Text } from 'react-native-paper'
+import { useSelector } from 'react-redux'
 import { DetailPayment } from './DetailPayment'
 import { ModalPayment } from './ModalPayment'
 import { WarnPayment } from './components'
 
 const PaymentScreen = () => {
   const route = useRoute<RouteBookingStackType<'BOOKING_CONFIRM'>>()
+  const navigation = useNavigation<NavigationProp>()
   const [openModal, setModal] = useState(false)
   const totalPriceString = route.params.seats
     .reduce((total, seat) => total + seat.price, 0)
@@ -28,12 +36,84 @@ const PaymentScreen = () => {
     },
   )
 
+  const { user } = useSelector(
+    ({ auth }: RootStore) => ({
+      user: auth.user,
+    }),
+    isEqual,
+  )
+
   const handleOpenModal = () => {
     setModal(true)
   }
 
   const handleCloseModal = () => {
     setModal(false)
+  }
+
+  const createPaymentIntent = useMutation(createIntent, {
+    onError: () => {
+      Alert.alert('Thông báo', 'Khoi tạo thanh toán thất bại!')
+    },
+  })
+
+  const mutate = useMutation(createTransaction, {
+    onSuccess: () => {
+      Alert.alert('Thông báo', 'Đặt vé thành công!', [
+        {
+          text: 'OK',
+          onPress: () => {
+            navigation.navigate('BottomTabs', { screen: 'TAB_HOME' })
+          },
+        },
+      ])
+    },
+    onError: (error) => {
+      console.log(error)
+    },
+  })
+
+  const { initPaymentSheet, presentPaymentSheet } = useStripe()
+
+  const onTransaction = async () => {
+    const totalPrice = route.params.seats.reduce((total, seat) => total + seat.price, 0)
+
+    const { paymentIntent: clientSecret } = await createPaymentIntent.mutateAsync({
+      amount: totalPrice,
+    })
+
+    if (!clientSecret) Alert.alert('Thông báo', 'Khởi tạo thanh toán thất bại!')
+
+    const { error: errorInitPayment } = await initPaymentSheet({
+      merchantDisplayName: 'Movie App',
+      paymentIntentClientSecret: clientSecret,
+      returnURL: 'movieapp://stripe-redirect',
+      defaultBillingDetails: {
+        name: user?.last_name,
+        phone: user?.phone_number,
+        email: user?.email,
+      },
+    })
+
+    if (errorInitPayment) {
+      Alert.alert('Thông báo', 'Khởi tạo tạo thanh toán thất bại!')
+      return
+    }
+
+    const { error } = await presentPaymentSheet()
+
+    if (error) {
+      return
+    }
+
+    mutate.mutate({
+      schedule_id: route.params.schedule_id,
+      seats: route.params.seats.map((seat) => seat.id),
+      user_id: user?.id as string,
+      payment_type: 'VISA',
+      price: totalPrice,
+      status: 'SUCCESS',
+    })
   }
 
   return (
@@ -57,7 +137,7 @@ const PaymentScreen = () => {
               <Text style={{ fontWeight: '700' }}>{totalPriceString} đ</Text>
             </View>
 
-            <Button mode="contained" style={{ borderRadius: 10 }} onPress={handleOpenModal}>
+            <Button mode="contained" style={{ borderRadius: 10 }} onPress={onTransaction}>
               XÁC NHẬN
             </Button>
           </View>
